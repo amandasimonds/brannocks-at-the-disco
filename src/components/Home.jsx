cb4472a9143d271b936f573f5d90f3ca3d71bf5e
Amanda Simonds <amandasimonds9@gmail.com>
2023-05-26 18:56:39 -0400

    made picture bigger on mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,14 @@ import LeavesVector from '../assets/Leaves Vector.svg';
 const Home = () => {
 	return (
 		<div>
-			<div className='overflow-hidden'>
+			<div className='h-[40vh] md:h-auto overflow-hidden'>
 				<img
 					src={BannerPic}
-					className="animate-fade-in md:object-cover md:scale-[1] object-cover scale-[2] top-[-30px] relative"
+					className="animate-fade-in md:object-cover md:scale-[1] object-cover scale-[3] md:top-0 top-[20px] relative"
 					alt="Amanda and Ben in the Rocky Mountains"
 				/>
 			</div>
-			<div className="animate-slide-in-left lg:scale-2 lg:top-[700px] md:w-[60vw] w-[30vw] absolute right-0 md:top-[480px] top-[160px]">
+			<div className="animate-slide-in-left lg:scale-2 lg:top-[700px] md:w-[60vw] w-[30vw] absolute right-0 md:top-[480px] top-[260px]">
 				<img
 					src={LeavesVector}
 					alt="White leaves outline"
