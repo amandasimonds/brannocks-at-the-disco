d6c105154f400737e030a0c473978c06ffa20930
Amanda Simonds <amandasimonds9@gmail.com>
2023-09-12 16:21:34 -0400

    save auth to local storage, enter key, better flow

--- src/components/PasswordProtect.jsx
+++ src/components/PasswordProtect.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const PasswordProtect = (props) => {
+	useEffect(() => {
+		checkAuth();
+	}, []);
 
 	const [authorized, setAuthorized] = useState(false);
 	const [enteredPassword, setEnteredPassword] = useState('');
@@ -9,21 +12,33 @@ export const PasswordProtect = (props) => {
 		setEnteredPassword(event.target.value);
 	};
 
+	const checkAuth = () => {
+		const auth = localStorage.getItem('auth');
+		auth ? setAuthorized(true) : setAuthorized(false);
+	};
+
 	const checkPassword = (event) => {
 		event.preventDefault();
-		console.log(enteredPassword);
-
 		if (enteredPassword === 'intothewoods') {
-			setAuthorized(true);
-			console.log('hooray');
+			localStorage.setItem('auth', true);
+			checkAuth();
 		} else {
+			alert(
+				`You entered the wrong password. Need the password? Email amandasimonds9@gmail.com with your name and phone number, and I'll get it to you soon`
+			);
 			setAuthorized(false);
 		}
 	};
 
-	// if (!authorized) {
-	// 	console.log('not authorized');
-	return !authorized ? (
+	const handleEnterKeyDown = (event) => {
+		if (event.key === 'Enter') {
+			checkPassword(event);
+		}
+	};
+
+	return authorized ? (
+		<div>{props.children}</div>
+	) : (
 		<div className="flex justify-center pt-5 pb-5">
 			<div className="flex flex-col items-center gap-4 w-10/12 bg-greenlightest p-4 rounded">
 				Please enter the guest password
@@ -32,14 +47,13 @@ export const PasswordProtect = (props) => {
 					className="input"
 					value={enteredPassword}
 					onChange={passwordInputChangeHandler}
+					onKeyDown={handleEnterKeyDown}
 				/>
 				<button className="btn" type="submit" onClick={checkPassword}>
 					Enter
 				</button>
 			</div>
 		</div>
-	) : (
-		<p>{props.children}</p>
 	);
 };
 // console.log(authorized);
