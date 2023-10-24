2567dc2173e55a25b542953a89505102d5539032
Amanda Simonds <amandasimonds9@gmail.com>
2023-10-24 17:32:47 -0400

    change 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,6 +24,9 @@ const Navbar = () => {
 						<a href="#our-wedding">accomodations</a>
 					</li>
 					<li>
+						<a href="#our-wedding">accomodations</a>
+					</li>
+					<li>
 						<a href="#our-wedding">rsvp</a>
 					</li>
 					<li>
